ba8c6a21e46033c09cf738ddf057b8c36e0323b0
Yuriy <1536549+husa@users.noreply.github.com>
2025-02-21 03:47:10 +0200

    close popup window on focus lost (#72)
    * close popup window on focus lost
    
    closes #71
    
    * add augroup to clear previous handlers

--- lua/snipe/menu.lua
+++ lua/snipe/menu.lua
@@ -263,6 +263,15 @@ function Menu:open(items, tag_followed, fmt, preselect)
   end
 
   self.old_tags = tags
+
+  vim.api.nvim_create_autocmd("WinLeave", {
+    group = vim.api.nvim_create_augroup("SnipeMenu", { clear = true }),
+    callback = function()
+      self:close()
+    end,
+    desc = "Close Snipe menu on window leave",
+    once = true,
+  })
 end
 
 function Menu:open_over()
